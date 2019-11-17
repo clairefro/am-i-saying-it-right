25f70312f7d32dce892b7b58ce15730642958a3f
claire froelich <claire.froelich@gmail.com>
2019-11-17 14:59:52 -0500

    added comma

--- speechToText.js
+++ speechToText.js
@@ -11,7 +11,7 @@ const recognizeSpeech = async (lang) => {
     recognition.start();
     console.log('speech rec init');
 
-    // on result of recognition, resolve promise with the transcript if captured
+    // on result of recognition, resolve promise with the transcript, if captured
     recognition.addEventListener('result', (event) => {
       if (event.results.length > 0) {
         resolve(event.results[0][0].transcript);
