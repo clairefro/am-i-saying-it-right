4e13c456a727d35dcbda37500bfe6524f90760b4
claire froelich <claire.froelich@gmail.com>
2020-01-16 23:18:51 -0500

    finishing touches

--- speechToText.js
+++ speechToText.js
@@ -1,47 +1,44 @@
-const toggleListeningAnimation = (verb) => {
-  const speakButton = document.getElementById('stt-button');
-  speakButton.classList[verb]('listening');
-  speakButton.classList[verb]('animated');
-  speakButton.classList[verb]('infinite');
-  speakButton.classList[verb]('pulse');
-}
+import toggleListeningAnimation from './animate_button.js';
 
 // get transcript from user speech
 const recognizeSpeech = async (lang) => {
-  // make a promise, to return text transcript when results processed and ready
-  const promise = new Promise((resolve, reject) => {
-    // create new instance of SpeechRecognition
-    const recognition = new webkitSpeechRecognition(); // Chrome only
-    // config language
-    recognition.lang = lang;
+  // check browser compatibility (Chrome only in this config)
+  if (!window.webkitSpeechRecognition) {
+    alert('Your browser doesn\'t support speech to text.\nTry Chrome 33+ :)');
+  } else {
+    // make a promise, to return text transcript when results processed and ready
+    const promise = new Promise((resolve, reject) => {
+      // create new instance of SpeechRecognition
+      const recognition = new webkitSpeechRecognition();
+      // config recognition language
+      recognition.lang = lang;
 
-    // toggle animation of button on start/end
-    recognition.onstart = (e) => {
-      console.log(e);
-      toggleListeningAnimation('add');
-    }
-
-    recognition.onend = () => {
-      toggleListeningAnimation('remove');
-    }
-
-    // turn on mic and start recognizing user speech
-    recognition.start();
-
-    // on result of recognition, resolve promise with the transcript, if captured
-    recognition.addEventListener('result', (event) => {
-
-      if (event.results.length > 0) {
-        resolve(event.results[0][0].transcript);
+      // toggle animation of button on start/end
+      recognition.onstart = (e) => {
+        console.log(e);
+        toggleListeningAnimation('add');
       }
-      else {
-        // or print this to console if something went wrong...
-        reject('try again');
+      recognition.onend = () => {
+        toggleListeningAnimation('remove');
       }
+      // turn on mic and start recognizing user speech
+      recognition.start();
+
+      // on result of recognition, resolve promise with the transcript, if captured
+      recognition.addEventListener('result', (event) => {
+
+        if (event.results.length > 0) {
+          resolve(event.results[0][0].transcript);
+        }
+        else {
+          // or print this to console if something went wrong...
+          reject('try again');
+        }
+      });
     });
-  });
-  // return the result of fulfilled promise
-  return await promise;
+    // return the result of fulfilled promise
+    return await promise;
+  }
 };
 
 export { recognizeSpeech };
--- textToSpeech.js
+++ textToSpeech.js
@@ -1,16 +1,20 @@
 // define a method to speak text in specified language
 const speakText = (text, lang) => {
-  // speechSynthesis only "speaks" SpeechSynthesisUtterance's. Let's make one.
-  const u = new SpeechSynthesisUtterance();
+  // check browser compatibility
+  if (window.speechSynthesis) {
+    alert('Your browser doesn\'t support text to speech.\nTry Chrome 33+ :)');
+  } else {
+    // speechSynthesis only "speaks" SpeechSynthesisUtterance's. Let's make one.
+    const u = new SpeechSynthesisUtterance();
 
-  // configure our new utterance
-  u.text = text;
-  u.lang = lang;
-  // u.rate = 1;  // optional: change speaking speed (0.1 [slow] - 1 [normal])
+    // configure our new utterance
+    u.text = text;
+    u.lang = lang;
+    // u.rate = 1;  // optional: change speaking speed (0.1 [slow] - 1 [normal])
 
-  // finally, call speechSynthesis.speak(utterance) to make the browser speak!
-  speechSynthesis.speak(u);
+    // finally, call speechSynthesis.speak(utterance) to make the browser speak!
+    speechSynthesis.speak(u);
+  }
 }
 
 export { speakText };
-
